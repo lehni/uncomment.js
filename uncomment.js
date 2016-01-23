760b68ec71d151ff1eb1b3f67d090fee8a50552f
Jürg Lehni <juerg@scratchdisk.com>
2016-01-23 16:31:15 +0100

    Improve string processing and filtering performance.
    Keep track of valid ranges rather than processing each char separately.
    
    Also closes #1

--- uncomment.js
+++ uncomment.js
@@ -4,32 +4,30 @@
  * Copyright (c) 2011 - 2016 Juerg Lehni
  * http://scratchdisk.com/
  *
- * Based on, and expanded from:
- * http://james.padolsey.com/javascript/removing-comments-in-javascript/
- *
  * Distributed under the MIT license.
  */
 
 function uncomment(str, options) {
 	options = options || {};
-	// Add some padding so we can always look ahead and behind by two chars
-	str = ('__' + str + '__').split('');
-	var quote = false,
-		quoteSign,
+	var length = str.length,
+		parts = [],
+		index = 0,
+		quote = false,
+		quoteChar,
 		regularExpression = false,
 		characterClass = false,
 		blockComment = false,
 		lineComment = false,
 		preserveComment = false;
 
-	for (var i = 0, l = str.length; i < l; i++) {
+	for (var i = 0; i < length; i++) {
 		// When checking for quote escaping, we also need to check that the
-		// escape sign itself is not escaped, as otherwise '\\' would cause
-		// the wrong impression of an unclosed string:
+		// escape sign itself is not escaped, as otherwise '\\' would cause the
+		// wrong impression of an unclosed string:
 		var unescaped = str[i - 1] !== '\\' || str[i - 2] === '\\';
 
 		if (quote) {
-			if (str[i] === quoteSign && unescaped)
+			if (str[i] === quoteChar && unescaped)
 				quote = false;
 		} else if (regularExpression) {
 			// Make sure '/'' inside character classes is not considered the end
@@ -44,51 +42,59 @@ function uncomment(str, options) {
 		} else if (blockComment) {
 			// Is the block comment closing?
 			if (str[i] === '*' && str[i + 1] === '/') {
-				if (!preserveComment)
-					str[i] = str[i + 1] = '';
-				blockComment = preserveComment = false;
 				// Increase by 1 to skip closing '/', as it would be mistaken
-				// for a regexp otherwise
+				// for a regexp otherwise.
 				i++;
-			} else if (!preserveComment) {
-				str[i] = '';
+				if (!preserveComment) {
+					// Next content starts after the comment.
+					index = i + 1;
+				}
+				blockComment = preserveComment = false;
 			}
 		} else if (lineComment) {
-			// One-line comments end with the line-break
-			if (/[\n\r]/.test(str[i + 1]))
+			// One-line comments end with the line-break.
+			if (/[\n\r]/.test(str[i + 1])) {
 				lineComment = false;
-			str[i] = '';
+				// Next content starts after the comment.
+				index = i + 1;
+			}
 		} else {
 			if (/['"]/.test(str[i])) {
 				quote = true;
-				quoteSign = str[i];
+				// Remember the type of opening quote so we can match it.
+				quoteChar = str[i];
 			} else if (str[i] === '/') {
-				if (str[i + 1] === '*') {
+				var next = str[i + 1];
+				if (next === '*') {
 					// Do not filter out conditional comments /*@ ... */
 					// and comments marked as protected /*! ... */
 					preserveComment = /[@!]/.test(str[i + 2]);
-					if (!preserveComment)
-						str[i] = '';
 					blockComment = true;
-				} else if (str[i + 1] === '/') {
-					str[i] = '';
+				} else if (next === '/') {
 					lineComment = true;
 				} else {
 					// We need to make sure we don't count normal divisions as
-					// regular expresions. Matching this properly is difficult,
+					// regular expressions. Matching this properly is difficult,
 					// but if we assume that normal division always have a space
 					// after /, a simple check for white space or '='' (for /=)
 					// is enough to distinguish divisions from regexps.
 					// TODO: Develop a proper check for regexps.
-					if (!/[\s=]/.test(str[i + 1])) {
-						regularExpression = true;
-					}
+					regularExpression = !/[\s=]/.test(next);
+				}
+				if (index < i && (lineComment ||
+						blockComment && !preserveComment)) {
+					// Insert valid content up to the comment.
+					parts.push(str.substring(index, index = i));
 				}
 			}
 		}
 	}
-	// Remove padding again.
-	str = str.join('').slice(2, -2);
+	if (index < length && !lineComment && (!blockComment || preserveComment)) {
+		// Insert valid content up to the end of the string.
+		parts.push(str.substring(index, length));
+	}
+	// Convert back to one string, by concatenating the valid parts.
+	str = parts.join('');
 
 	if (options.removeEmptyLines || options.mergeEmptyLines) {
 		// Strip empty lines that contain only white space and line breaks, as
